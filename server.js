7a0c315961663ebd88202a625b475d345d175303
kb0303 <bishtkrishnam400@gmail.com>
2024-09-03 02:24:38 +0530

    changes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// websocket-server.js
 import { WebSocketServer, WebSocket } from 'ws';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -9,18 +8,19 @@ app.use(bodyParser.json());
 const wss = new WebSocketServer({ noServer: true });
 
 app.post('/broadcast', (req, res) => {
-  const { message } = req.body;
-  if (message) {
+  const { type, content } = req.body;
+  if (type && content) {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(message));
+        client.send(JSON.stringify({ type, content }));
       }
     });
     res.status(200).json({ status: 'Message broadcasted' });
   } else {
-    res.status(400).json({ error: 'Message is required' });
+    res.status(400).json({ error: 'Type and content are required' });
   }
 });
+
 const port = 8080;
 const server = app.listen(port, () => {
   console.log(`HTTP server running on http://localhost:${port}`);
